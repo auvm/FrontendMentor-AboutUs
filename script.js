e7841b8e71d6e055ce7ceef1c65acccec8b362e7
auvm <angelurielvelascom@gmail.com>
2024-06-19 13:24:45 -0600

    add final mobile design

---
+++ script.js
@@ -0,0 +1,7 @@
+let btn_sumit = document.getElementById("btn_submit");
+
+// btn_sumit.addEventListener(onclick());
+
+btn_sumit.onclick = function(){
+    alert("Message Sent!\nThanks for completing the form. We'll be in touch soon!");
+};
